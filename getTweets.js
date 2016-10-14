f70d46de8bafb96ac41d5d8fccd77b2b157ce24e
addy1110 <addy.1110@gmail.com>
2016-10-14 16:58:19 -0400

    adding dependent files

--- getTweets.js
+++ getTweets.js
@@ -3,16 +3,27 @@ var fs = require("fs"),
  	options = require('./config.js'),
  	Twit = require('twit');
 
-var fetch = new Twit({
-  consumer_key:options.storageConfig.consumer_key,
-  consumer_secret:options.storageConfig.consumer_secret ,
-  access_token:options.storageConfig.access_token,
-  access_token_secret:options.storageConfig.access_token_secret
+var fetch;
+fetch = new Twit({
+    consumer_key: options.storageConfig.consumer_key,
+    consumer_secret: options.storageConfig.consumer_secret,
+    access_token: options.storageConfig.access_token,
+    access_token_secret: options.storageConfig.access_token_secret
 });
 
 
  //Pushing tweets into elastic search.
-var hashtags = 'trump,election,hillary,music,sports,weather,movie,technology,twitter,facebook,instagram,snapchat,hell,iphone';
+var hashtags;
+hashtags =  'election,trump,hillary,vote,clinton,democrats,republicans,debate,voting,elections' +
+            'games,sports,fifa,espn,racing,race,cricket,soccer,football,nba,baseball,sports,basketball,golf,swimming,' +
+            'entertainment,music,movie,song,songs,broadway,hollywood,bollywood,selfie,' +
+            'health,gym,exercise,fitness,yoga,healthy,fit,nutrition,wellness,ill' +
+            'technology,apple,mac,gadget,samsung,iphone,sony,mobile,science,engineering,tech,robotics' +
+            'travel,travelling,journey,trip,roadtrip,tour,driving,flying,transit,trekking,riding,bikig,cruising,weekend' +
+            'religion,faith,god,jesus,spirituality,church,humanity,peace,worship,prayer,ritual' +
+            'food,restraunt,lunch,dinner,breakfast,foodie,pizza,dining,tasty,eat,eating,recipes,wine,bar' +
+            'fashion,style,models,magazine,playboy,clothes,dress,trend,gucci,halloween,brand' +
+            'facebook,snapchat,youtube,social,media';
 var world = [ '-180', '-90', '180', '90' ];
 var stream = fetch.stream('statuses/filter', {track: hashtags}, { locations: world});
 var count=0;
@@ -22,7 +33,7 @@ var count=0;
   stream.on('tweet', function(tweet) {
   	if (tweet.geo !=null){
   		count+=1;
-  		console.log("tweets counted:"+count);
+  		console.log("tweets counted:"+count+" tweet: "+tweet.text);
   		request({
   			url: 'https://search-es-twitter-tweets-xvyjqon7dx2pquurvjs45mirti.us-east-1.es.amazonaws.com/domain/tweets',
       		method: "POST",
